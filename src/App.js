a8a7f462aba2be7fddb0324e4f01d382bae1aa08
shivanshi0526 <shivanshi0526@gmail.com>
2024-04-06 18:34:48 +0530

    basic routes created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Login, Home as Lobby, LeaderBoard, Game, WaitingList } from './Web/Componets/Pages';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-         Rock paper scissor 
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/lobby" element={<Lobby />} />
+        <Route path="/game" element={<Game />} />
+        <Route path="/leaderboard" element={<LeaderBoard />} />
+        <Route path="/waiting-list" element={<WaitingList />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/Web/Componets/Pages/index.js
@@ -0,0 +1,13 @@
+import Login from "./Login";
+import Home from "./Home";
+import LeaderBoard from "./LeaderBoard";
+import Game from "./Game";
+import WaitingList from "./WaitingList";
+
+export{
+    Home,
+    Login,
+    LeaderBoard,
+    Game,
+    WaitingList
+}
