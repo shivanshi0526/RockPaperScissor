ce4cd35d3e7a9927d1a00daa7bb4f1dd140afb6d
shivanshi0526 <shivanshi0526@gmail.com>
2024-04-06 15:31:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         Rock paper scissor 
         </a>
       </header>
     </div>
